79bd05fdd6559fcb5f32fa0b4f8120c01fc09e18
santosdiiih <aparecida.rpsilva@gmail.com>
2020-10-02 09:48:35 -0300

    layout do card de publicacoes

---
+++ mobile/app/src/main/java/com/example/webgamescenter/model/Card-Jogo.kt
@@ -0,0 +1,10 @@
+package com.example.webgamescenter.model
+
+class `Card-Jogo`(
+    //var imagem: String,
+    var code: Int,
+    var name: String,
+    var date: Int,
+    var text: String,
+    var rate: Int
+)
---
+++ mobile/app/src/main/java/com/example/webgamescenter/ui/index.kt
@@ -0,0 +1,13 @@
+package com.example.webgamescenter.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.webgamescenter.R
+
+class index : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_index)
+    }
+}
