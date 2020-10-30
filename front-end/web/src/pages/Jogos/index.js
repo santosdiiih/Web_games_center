835f7b73796504156c32e3c132abe97f0f545972
cleyson.ribeiro.777@gmail.com <cleyson.ribeiro.777@gmail.com>
2020-10-30 11:33:02 -0300

    Criação de usuário pela web e login

--- front-end/web/src/pages/Jogos/index.js
+++ front-end/web/src/pages/Jogos/index.js
@@ -1,51 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect, Component } from "react";
 import { Link } from 'react-router-dom';
 import { Form } from 'reactstrap';
 import Foto from '../../assets/Raster.jpeg';
 
 import './styles.css';
+import { apiJogos } from "../../services/api";
+import Axios from "axios";
 
-function home() {
+function Home() {
 
-    
-  return (
-      <>
-        <div className="body">
-            <header>
-                <div className="containernavegacao">
-                    <div className="Container pesquisa-genero">
-                        <input type="text" className="text-input" placeholder="Pesquise seu Jogo"></input>
-                        <select className="select-input">
-                            <option>Selecione outra categoria</option>
-                        </select>
+
+    useEffect(() =>{
+        const retorno = Axios.get("https://api.rawg.io/api/games?key=e74d1dd729de47b6b16418e9c6b82fdd")
+        retorno.then(reponse=>{
+            console.log(reponse.data.results);
+        })
+        
+    },[]);
+
+
+    return (
+        <>
+            <div className="body">
+                <header>
+                    <div className="containernavegacao">
+                        <div className="Container pesquisa-genero">
+                            <input type="text" className="text-input" placeholder="Pesquise seu Jogo"></input>
+                            <select className="select-input">
+                                <option>Selecione outra categoria</option>
+                            </select>
+                        </div>
+                        <div className="">
+                            <nav className="navegacao">
+                                <ul className="ul-menu-nav">
+                                    <li className="li-menu-navbar">Jogos Mobile</li>
+                                    <Link to="/"><li className="li-menu-navbar">Home</li></Link>
+                                    <Link to="/perfil"><li className="li-menu-navbar">Perfil</li></Link>
+                                </ul>
+                            </nav>
+                        </div>
                     </div>
-                 <div className="">
-                    <nav className="navegacao">
-                        <ul className="ul-menu-nav">
-                            <li className="li-menu-navbar">Jogos Mobile</li>
-                            <Link to="/"><li className="li-menu-navbar">Home</li></Link>
-                            <Link to="/perfil"><li className="li-menu-navbar">Perfil</li></Link>
-                        </ul>
-                    </nav>
-                 </div>
-                </div>
-            </header>
-            <section className="container">
-               <div className="container-card-jogo">
-                    <div className="card-jogo">
-                        <div className="card-header-jogo text-center text-white"><h6 className="mt-2">GOD OF WAR</h6></div>
-                        <div className="card-body-jogo text-white"> <img src={Foto} alt="" className="img-type-size" /> </div>
-                        <div className="card-footer-jogo">
-                            <div className="text-center">
-                                <button className="input-button">Entrar</button>
+                </header>
+                <section className="container">
+                    <div className="container-card-jogo">
+                        <div className="card-jogo">
+                            <div className="card-header-jogo text-center text-white"><h6 className="mt-2">GOD OF WAR</h6></div>
+                            <div className="card-body-jogo text-white"> <img src={Foto} alt="" className="img-type-size" /> </div>
+                            <div className="card-footer-jogo">
+                                <div className="text-center">
+                                    <button className="input-button">Entrar</button>
+                                </div>
                             </div>
                         </div>
                     </div>
-               </div>
-            </section>
-        </div>
-      </>
-  );
+                </section>
+            </div>
+        </>
+    );
 }
 
-export default home;
+export default Home;
