6b10d049191eb08034b85322b87338f6c33241c8
afonso <peresfon@gmail.com>
2020-11-27 10:51:43 -0300

    Versão front-end atualizado

---
+++ web/src/pages/Jogos/index.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect, Component } from "react";
+import { Link } from 'react-router-dom';
+import { Form } from 'reactstrap';
+import Foto from '../../assets/Raster.jpeg';
+
+import './styles.css';
+import { apiJogos } from "../../services/api";
+import Axios from "axios";
+
+function Home() {
+
+
+    useEffect(() =>{
+        const retorno = Axios.get("https://api.rawg.io/api/games?key=e74d1dd729de47b6b16418e9c6b82fdd")
+        retorno.then(reponse=>{
+            
+            console.log(reponse.data.results);
+        })
+        
+    },[]);
+
+
+    return (
+        <>
+            <div className="body">
+                <header>
+                    <div className="containernavegacao">
+                        <div className="Container pesquisa-genero">
+                            <input type="text" className="text-input" placeholder="Pesquise seu Jogo"></input>
+                            <select className="select-input">
+                                <option>Selecione outra categoria</option>
+                            </select>
+                        </div>
+                        <div className="">
+                            <nav className="navegacao">
+                                <ul className="ul-menu-nav">
+                                    <li className="li-menu-navbar">Jogos Mobile</li>
+                                    <Link to="/"><li className="li-menu-navbar">Home</li></Link>
+                                    <Link to="/perfil"><li className="li-menu-navbar">Perfil</li></Link>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                </header>
+                <section className="container">
+                    <div className="container-card-jogo">
+                        <div className="card-jogo">
+                            <div className="card-header-jogo text-center text-white"><h6 className="mt-2">GOD OF WAR</h6></div>
+                            <div className="card-body-jogo text-white"> <img src={Foto} alt="" className="img-type-size" /> </div>
+                            <div className="card-footer-jogo">
+                                <div className="text-center">
+                                    <button className="input-button">Entrar</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </>
+    );
+}
+
+export default Home;
---
+++ web/src/pages/Perfil/index.js
@@ -0,0 +1,172 @@
+import React, { useState, useEffect } from 'react';
+import { Button, Col, Form, FormGroup, FormText, Input, Label, Row } from 'reactstrap';
+import Foto from '../../assets/logo-type-web.png';
+import FotoPerfil from '../../assets/Perfil.jpg';
+import Menu from '../Components/ComponentsMenu/Menu';
+import Menutopside from '../Components/Menutopside';
+
+
+import './styles.css';
+import { getUsuario } from '../../services/security';
+import { api } from '../../services/api';
+
+function Perfil() {
+
+    const usuarioSessao = getUsuario();
+
+    const [usuarioRegistrar, setUsuarioRegistrar] = useState({
+        email: usuarioSessao.email,
+        senha: "",
+        primeiro_nome: usuarioSessao.primeiro_nome,
+        ultimo_nome: usuarioSessao.ultimo_nome,
+        nickname: usuarioSessao.nickname,
+        sexo_id: "",
+        estado_id: "",
+        confirmesenha: ""
+    });
+
+    const [sexos, setSexos] = useState([]);
+    const [estados, setEstados] = useState([]);
+    
+    const handlerInput = (e) => {
+        setUsuarioRegistrar({ ...usuarioRegistrar, [e.target.id]: e.target.value });
+    }
+
+
+    useEffect(() => {
+        const listarSexos = async () => {
+            try {
+                const retorno = await api.get("/sexo");
+
+                setSexos(retorno.data)
+
+                console.log(retorno);
+
+            } catch (e) {
+                console.log(e)
+            }
+        }
+
+        listarSexos();
+    }, [])
+
+    useEffect(() => {
+        const listarEstados = async () => {
+            try {
+                const retorno = await api.get("/estados")
+
+                setEstados(retorno.data);
+            } catch (e) {
+                console.log(e)
+            }
+        }
+        listarEstados();
+    }, [])
+
+    return <>
+        <div className="page-wraper">
+            <div className="content-wraper">
+                <div className="background-menu-lateral">
+                    <Menu />
+                </div>
+                <div className="content-area">
+                    <Menutopside />
+                    <div className="container-perfil">
+                        <section>
+                            <div className="perfil-jogos">
+                                <div className="container-jogo-perfil">
+                                    <div><img src={FotoPerfil} className="foto-jogo-perfil" alt="" /></div>
+                                    <div className="perfil-jogo-container-text text-white"><h6 className="text-center ">Joguinho 1</h6></div>
+                                    <div className="svg-normal">
+                                        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="sign-in-alt"  role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                                            <path fill="currentColor" d="M416 448h-84c-6.6 0-12-5.4-12-12v-40c0-6.6 5.4-12 12-12h84c17.7 0 32-14.3 32-32V160c0-17.7-14.3-32-32-32h-84c-6.6 0-12-5.4-12-12V76c0-6.6 5.4-12 12-12h84c53 0 96 43 96 96v192c0 53-43 96-96 96zm-47-201L201 79c-15-15-41-4.5-41 17v96H24c-13.3 0-24 10.7-24 24v96c0 13.3 10.7 24 24 24h136v96c0 21.5 26 32 41 17l168-168c9.3-9.4 9.3-24.6 0-34z">
+                                            </path>
+                                        </svg>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                        <section>
+                            <div className="perfil-perfil">
+                                <div className="perfil">
+                                    <div className="">
+                                        <img src={FotoPerfil} alt="img perfil" className="img-perfil-foto mt-4" />
+                                    </div>
+                                    <div className="perfil-input">
+                                        <Form>
+                                           <Row>
+                                                <Col md={6}>
+                                                    <FormGroup>
+                                                        <Label for="nome" className="text-white">Primeiro Nome</Label>
+                                                        <Input type="text" name="name" id="primeiro_nome" placeholder="Seu nome" value={usuarioRegistrar.primeiro_nome} onChange={handlerInput}/>
+                                                    </FormGroup>
+                                                </Col>
+                                                <Col md={6}>
+                                                    <FormGroup>
+                                                        <Label for="nome" className="text-white">Ultimo Nome</Label>
+                                                        <Input type="text" name="name" id="ultimo_nome" placeholder="Seu nome" value={usuarioRegistrar.ultimo_nome} onChange={handlerInput}/>
+                                                    </FormGroup>
+                                                </Col>
+                                                <Col md={6}>
+                                                    <FormGroup>
+                                                        <Label for="nickname" className="text-white">Nick-Name</Label>
+                                                        <Input type="text" name="nickname" id="nickname" placeholder="Seu Nick-Name" value={usuarioRegistrar.nickname} onChange={handlerInput} />
+                                                    </FormGroup>
+                                                </Col>
+                                                <Col md={6}>
+                                                    <FormGroup>
+                                                        <Label for="nickname" className="text-white">Senha</Label>
+                                                        <Input type="text" name="nickname" id="nickname" placeholder="Sua senha" value={usuarioRegistrar.senha} onChange={handlerInput}/>
+                                                    </FormGroup>
+                                                </Col>
+                                                <Col md={6}>
+                                                    <FormGroup>
+                                                        <Label for="nickname" className="text-white">Ano De Nascimento</Label>
+                                                        <Input type="text" name="nickname" id="nickname" placeholder="Ano de Nascimento"/>
+                                                    </FormGroup>
+                                                </Col>
+                                                <Col md={6}> 
+                                                    <FormGroup>
+                                                        <Label for="E-mail" className="text-white">E-Mail</Label>
+                                                        <Input type="email" name="E-mail" id="email" placeholder="Seu nome" value={usuarioRegistrar.email} onChange={handlerInput}/>
+                                                    </FormGroup>
+                                                </Col>
+                                                <Col md={6}>
+                                                    <FormGroup>
+                                                        <Label for="sexo" className="text-white">Selecione seu sexo</Label>
+                                                        <Input onChange={handlerInput} type="select" name="sexo" id="sexo_id">
+                                                            <option value="">Selecione</option>
+                                                            {sexos.map(o => (
+                                                                <option value={o.id}>{o.nome}</option>
+                                                            ))}
+                                                        </Input>
+                                                    </FormGroup>
+                                                </Col>
+                                                <Col md={6}>
+                                                    <FormGroup>
+                                                        <Label for="categoria" className="text-white">Selecione seu estado</Label>
+                                                        <Input onChange={handlerInput} type="select" name="categoria" id="estado_id">
+                                                            <option value="">Selecione seu estado</option>
+                                                            {estados.map(o => (
+                                                                <option value={o.id}>{o.nome}</option>
+                                                            ))}
+                                                        </Input>
+                                                    </FormGroup>
+                                                </Col>
+                                            </Row>
+                                            <div className="text-center">
+                                                <Button className="bg-primary text-center">Salvar Alterações</Button>
+                                            </div>
+                                        </Form>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>;
+}
+
+export default Perfil;
