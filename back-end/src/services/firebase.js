3d70db16fdace934936d458557fa9c1f04b22cf7
cleyson.ribeiro.777@gmail.com <cleyson.ribeiro.777@gmail.com>
2020-12-10 20:04:23 -0300

    Configuração do heroku

--- back-end/src/services/firebase.js
+++ back-end/src/services/firebase.js
@@ -9,17 +9,15 @@ admin.initializeApp({
         process.env.FIREBASE_PRIVATE_KEY
             ? {
                 type: "service_account",
-                project_id: "senai-overflow",
-                private_key_id: "73066f27e33601b0035117947f795a484b773efe",
-                private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+                project_id: "web-games-center",
+                private_key_id: "317f5b2502b9371cd143e488226624d1ffed48d3",
+                private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
                 client_email: process.env.FIREBASE_CLIENT_EMAIL,
-                client_id: "105469033790114328493",
+                client_id: "104126728326795526067",
                 auth_uri: "https://accounts.google.com/o/oauth2/auth",
                 token_uri: "https://oauth2.googleapis.com/token",
-                auth_provider_x509_cert_url:
-                    "https://www.googleapis.com/oauth2/v1/certs",
-                client_x509_cert_url:
-                    "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-k09tb%40senai-overflow.iam.gserviceaccount.com",
+                auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+                client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-r0g79%40web-games-center.iam.gserviceaccount.com"
             }
             : serviceAccount
     ),
